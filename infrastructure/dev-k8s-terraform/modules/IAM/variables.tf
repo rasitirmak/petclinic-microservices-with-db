d6d780d1f88ff1bfa49bbdc899b379a93e3f33b2
EC2 Default User <rasitirmak@yahoo.com>
2022-10-01 14:19:10 +0000

    terraform files added

---
+++ infrastructure/dev-k8s-terraform/modules/IAM/variables.tf
@@ -0,0 +1,29 @@
+}
+
+variable "worker-role-policy" {
+  default = "petclinic_policy_for_worker_role"
+}
+
+variable "master-role" {
+  default = "petclinic_role_master_k8s"
+}
+
+variable "worker-role" {
+  default = "petclinic_role_worker_k8s"
+}
+
+variable "master-role-attachment" {
+  default = "petclinic_attachment_for_master"
+}
+
+variable "worker-role-attachment" {
+  default = "petclinic_attachment_for_worker"
+}
+
+variable "profile_for_master" {
+  default = "petclinic_profile_for_master"
+}
+
+variable "profile_for_worker" {
+  default = "petclinic_profile_for_worker"
+}
